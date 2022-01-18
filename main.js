e351372da5bf65fd752ef8688831e1d9d30e0f3b
Developer Jahed <developerjahed@gmail.com>
2022-01-18 17:26:01 +0600

    LocalStorage data save

---
+++ main.js
@@ -0,0 +1,35 @@
+const addItem=()=>{
+   const productName=document.getElementById('productName');
+   const name=productName.value;
+
+// display in the ui
+   displayProduct(name);
+//    add to local storage
+addProductToCart(name);
+   productName.value='';
+}
+const displayProduct=name=>{
+    const ul=document.getElementById('products');
+    const li=document.createElement('li');
+    li.innerText=name;
+    ul.appendChild(li);
+}
+
+const getCart=()=>{
+   const cart= localStorage.getItem('cart');
+   let cartObj;
+   if(cart){
+     cartObj=JSON.parse(cart);
+   }else{
+       cartObj={};
+   }
+   return cartObj;
+}
+
+const addProductToCart=name=>{
+    const cart=getCart();
+    cart [name]=1;
+    console.log(cart);
+    const cartSrtingified=JSON.stringify(cart);
+    localStorage.setItem('cart', cartSrtingified);
+}
